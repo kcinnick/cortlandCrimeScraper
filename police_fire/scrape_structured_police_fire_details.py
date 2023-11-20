c3d8343e981ba3294fa77805ab03a1cc11721009
kcinnick <nicktucker4@gmail.com>
2023-11-20 14:36:10 -0500

    - add `clean_up_accused_record` to remove repeated code - add `scrape_separate_incident_details` to scrape incidents that are `</br>` tag formatted instead of `<strong> tag formatted

--- police_fire/scrape_structured_police_fire_details.py
+++ police_fire/scrape_structured_police_fire_details.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import regex as re
 from bs4 import BeautifulSoup
 from sqlalchemy.exc import IntegrityError
@@ -18,6 +20,149 @@ def identify_articles_with_incident_formatting(db_session):
     return articles_with_incidents
 
 
+def scrape_separate_incident_details(separate_incident_tags, article, DBsession):
+    separate_incident_tags = [i for i in separate_incident_tags if i.strip() != '']
+    accused_tag_index = 0
+    if 'Accused' in separate_incident_tags[accused_tag_index]:
+        pass
+    else:
+        accused_tag_index += 1
+        if 'Accused' in separate_incident_tags[accused_tag_index]:
+            pass
+        else:
+            print(f'No Accused found: {separate_incident_tags[accused_tag_index]}')
+            return
+
+    charges_tag_index = accused_tag_index + 1
+    if 'Charges' in separate_incident_tags[charges_tag_index]:
+        charges_str = separate_incident_tags[charges_tag_index]
+        pass
+    else:
+        print(f'No Charges found: {separate_incident_tags[charges_tag_index]}')
+        return
+
+    details_tag_index = charges_tag_index + 1
+    if 'Details' in separate_incident_tags[details_tag_index]:
+        details_str = separate_incident_tags[details_tag_index]
+        if 'Legal Actions' not in separate_incident_tags[details_tag_index + 1]:
+            details_str += separate_incident_tags[details_tag_index + 1]
+            legal_actions_tag_index = details_tag_index + 2
+        else:
+            legal_actions_tag_index = details_tag_index + 1
+        pass
+    else:
+        print(f'No Details found: {separate_incident_tags[details_tag_index]}')
+        return
+
+    if 'Legal Action' in separate_incident_tags[legal_actions_tag_index]:
+        legal_actions_str = separate_incident_tags[legal_actions_tag_index]
+    elif 'Legal actions' in separate_incident_tags[legal_actions_tag_index]:
+        legal_actions_str = separate_incident_tags[legal_actions_tag_index]
+    else:
+        print(f'No Legal Action found: {separate_incident_tags[legal_actions_tag_index]}')
+        return
+
+    accused_str = separate_incident_tags[accused_tag_index].replace('Accused: ', '')
+    accused_name, accused_age, accused_location = clean_up_accused_record(article, accused_str, DBsession)
+
+    # clean up charges, details, and legal actions records
+    if charges_str.startswith(': '):
+        charges_str = charges_str[2:]
+    else:
+        charges_str = charges_str.replace('Charges: ', '')
+    if details_str.startswith(': '):
+        details_str = details_str[2:]
+    else:
+        details_str = details_str.replace('Details: ', '')
+    if legal_actions_str.startswith(': '):
+        legal_actions_str = legal_actions_str[2:]
+    else:
+        legal_actions_str = re.sub(r'Legal [Aa]ctions: ', '', legal_actions_str)
+
+    incident = Incidents(
+        article_id=article.id,
+        url=article.url,
+        incident_reported_date=article.date_published,
+        accused_name=accused_name,
+        accused_age=accused_age,
+        accused_location=accused_location,
+        charges=charges_str,
+        details=details_str,
+        legal_actions=legal_actions_str,
+        structured_source=True
+    )
+
+    # add incident to database if it doesn't already exist
+    if DBsession.query(Incidents).filter_by(details=details_str).count() == 0:
+        try:
+            DBsession.add(incident)
+            DBsession.commit()
+        except IntegrityError as e:
+            print('Integrity error: ', e)
+            DBsession.rollback()
+
+    return
+
+
+def clean_up_accused_record(article, accused_str, DBsession):
+    # clean up accused record
+    accused_str = accused_str.replace(': ', '')
+    if ', Jr' in accused_str:
+        accused_str = accused_str.replace(', Jr.', ' Jr.')
+    if ', Sr' in accused_str:
+        accused_str = accused_str.replace(', Sr.', ' Sr.')
+
+    if ' and ' in accused_str:
+        accused_str = accused_str.replace(' and ', ';')
+
+    if ';' in accused_str:
+        print('; in accused_str')
+        incidentWithError = IncidentsWithErrors(
+            article_id=article.id,
+            url=article.url
+        )
+        try:
+            DBsession.add(incidentWithError)
+            DBsession.commit()
+        except IntegrityError as e:
+            DBsession.rollback()
+            raise e
+
+    accused_name = accused_str.split(',')[0].strip().split(' of ')[0].strip()
+    try:
+        accused_age = accused_str.split(',')[1].strip()
+    except IndexError:
+        accused_age = None
+    # if accused_age isn't a digit, it's probably a location
+    if accused_age and not accused_age.isdigit():
+        accused_age = None
+
+    if ' of ' in accused_name:
+        accused_location = accused_name.split(' of ')[1]
+    elif accused_age:
+        accused_location_index = 2
+        accused_location = None
+    else:
+        accused_location_index = 1
+        accused_location = None
+
+    if not accused_location:
+        try:
+            accused_location = ', '.join([i.strip() for i in accused_str.split(',')[accused_location_index:]])
+            if accused_location[-1] == '.':
+                accused_location = accused_location[:-1]
+            if accused_location.startswith('of '):
+                accused_location = accused_location[3:]
+        except IndexError:
+            if accused_location.strip() == '':
+                accused_location = None
+            else:
+                print(accused_location)
+                raise IndexError('accused_location was incorrectly formatted.')
+
+    return accused_name, accused_age, accused_location
+
+
 def scrape_structured_incident_details(article, DBsession):
     """
     Scrape incident details from article.
@@ -25,6 +170,8 @@ def scrape_structured_incident_details(article, DBsession):
     incidents = []
     print(article.url)
     soup = BeautifulSoup(article.html_content, 'html.parser')
+
+    # check for <strong> tags first
     accused = soup.find_all('strong', string=re.compile('Accused'))
     # for accused in accused:
     #     print(accused.next_sibling)
@@ -42,6 +189,19 @@ def scrape_structured_incident_details(article, DBsession):
     print(len(charges))
     print(len(details))
     print(len(legal_actions))
+
+    # if len of all of the above is 0, this article may have <br> tag formatting
+    if len(accused) == 0 and len(charges) == 0 and len(details) == 0 and len(legal_actions) == 0:
+        # check for <br> tag formatting
+        main_body = soup.find('div', class_='body main-body clearfix')
+        separate_incidents = main_body.find_all('p')
+        for separate_incident in separate_incidents:
+            br_tags = str(separate_incident).split('<br/>')
+            soupy_br_tags = [BeautifulSoup(br_tag, 'html.parser').text.strip() for br_tag in br_tags if
+                             br_tag.strip() != '']
+            pprint(soupy_br_tags)
+            scrape_separate_incident_details(soupy_br_tags, article, DBsession)
+        return
 
     if len(accused) != len(charges) or len(accused) != len(details) or len(accused) != len(legal_actions):
         incidentWithError = IncidentsWithErrors(
@@ -91,59 +251,7 @@ def scrape_structured_incident_details(article, DBsession):
             continue
 
         # clean up accused record
-        accused_str = accused_str.replace(': ', '')
-        if ', Jr' in accused_str:
-            accused_str = accused_str.replace(', Jr.', ' Jr.')
-        if ', Sr' in accused_str:
-            accused_str = accused_str.replace(', Sr.', ' Sr.')
-
-        if ' and ' in accused_str:
-            accused_str = accused_str.replace(' and ', ';')
-
-        if ';' in accused_str:
-            print('; in accused_str')
-            incidentWithError = IncidentsWithErrors(
-                article_id=article.id,
-                url=article.url
-            )
-            try:
-                DBsession.add(incidentWithError)
-                DBsession.commit()
-            except IntegrityError:
-                DBsession.rollback()
-            continue
-
-        accused_name = accused_str.split(',')[0].strip().split(' of ')[0].strip()
-        try:
-            accused_age = accused_str.split(',')[1].strip()
-        except IndexError:
-            accused_age = None
-        # if accused_age isn't a digit, it's probably a location
-        if accused_age and not accused_age.isdigit():
-            accused_age = None
-
-        if ' of ' in accused_name:
-            accused_location = accused_name.split(' of ')[1]
-        elif accused_age:
-            accused_location_index = 2
-            accused_location = None
-        else:
-            accused_location_index = 1
-            accused_location = None
-
-        if not accused_location:
-            try:
-                accused_location = ', '.join([i.strip() for i in accused_str.split(',')[accused_location_index:]])
-                if accused_location[-1] == '.':
-                    accused_location = accused_location[:-1]
-                if accused_location.startswith('of '):
-                    accused_location = accused_location[3:]
-            except IndexError:
-                if accused_location.strip() == '':
-                    accused_location = None
-                else:
-                    print(accused_location)
-                    raise IndexError('accused_location was incorrectly formatted.')
+        accused_name, accused_age, accused_location = clean_up_accused_record(article, accused_str, DBsession)
 
         # clean up charges, details, and legal actions records
         if charges_str.startswith(': '):
@@ -167,12 +275,13 @@ def scrape_structured_incident_details(article, DBsession):
         )
 
         # add incident to database if it doesn't already exist
-        try:
-            DBsession.add(incident)
-            DBsession.commit()
-        except IntegrityError as e:
-            print('Integrity error: ', e)
-            DBsession.rollback()
+        if DBsession.query(Incidents).filter_by(details=details_str).count() == 0:
+            try:
+                DBsession.add(incident)
+                DBsession.commit()
+            except IntegrityError as e:
+                print('Integrity error: ', e)
+                DBsession.rollback()
 
     return
 
