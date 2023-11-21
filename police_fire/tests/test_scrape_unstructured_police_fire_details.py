c8fff4a09c5029a5219af8074c55bc8706dbc544
kcinnick <nicktucker4@gmail.com>
2023-11-21 15:01:09 -0500

    - add `delete_table_contents` to `test_scrape_unstructured_police_fire_details.py` - handle unstructured data responses as dicts

--- police_fire/tests/test_scrape_unstructured_police_fire_details.py
+++ police_fire/tests/test_scrape_unstructured_police_fire_details.py
@@ -1,4 +1,4 @@
-from database import get_database_session, Article, create_tables, Base, Incidents
+from database import get_database_session, Article, create_tables, Base, Incidents, IncidentsWithErrors
 from main import login, scrape_article
 
 from police_fire.scrape_structured_police_fire_details import scrape_structured_incident_details
@@ -7,15 +7,21 @@ from police_fire.scrape_unstructured_police_fire_details import scrape_unstructu
 create_tables(test=True)
 DBsession, engine = get_database_session(test=True)
 
+def delete_table_contents():
+    DBsession.query(IncidentsWithErrors).delete()
+    DBsession.query(Incidents).delete()
+    DBsession.query(Article).delete()
+    DBsession.commit()
+
 
 def test_duplicate_structured_incident_does_not_get_added():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+    delete_table_contents()
     incidents = DBsession.query(Incidents).all()
     assert len(incidents) == 0
 
     logged_in_session = login()
-    scrape_article('https://www.cortlandstandard.com/stories/virgil-man-charged-with-sex-abuse,70554?', logged_in_session,
+    scrape_article('https://www.cortlandstandard.com/stories/virgil-man-charged-with-sex-abuse,70554?',
+                   logged_in_session,
                    section='Police/Fire', DBsession=DBsession)
     test_article = DBsession.query(Article).where(
         Article.url == 'https://www.cortlandstandard.com/stories/virgil-man-charged-with-sex-abuse,70554?').first()
@@ -25,24 +31,32 @@ def test_duplicate_structured_incident_does_not_get_added():
 
     article_content = test_article.content
     article_date_published = test_article.date_published
-    scrape_unstructured_incident_details(test_article.id, test_article.url, article_content, article_date_published, DBsession)
+    scrape_unstructured_incident_details(test_article.id, test_article.url, article_content, article_date_published,
+                                         DBsession)
     incidents = DBsession.query(Incidents).all()
     assert len(incidents) == 5
 
+
 def test_multiple_unstructured_incidents_get_added():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+    delete_table_contents()
     incidents = DBsession.query(Incidents).all()
     assert len(incidents) == 0
 
     logged_in_session = login()
-    scrape_article('https://www.cortlandstandard.com/stories/brooklyn-man-found-with-5700-in-cocaine,33840', logged_in_session,
-                   section='Police/Fire', DBsession=DBsession)
-    test_article = DBsession.query(Article).where(
-        Article.url == 'https://www.cortlandstandard.com/stories/brooklyn-man-found-with-5700-in-cocaine,33840').first()
+    for article_url in ['https://www.cortlandstandard.com/stories/brooklyn-man-found-with-5700-in-cocaine,33840',
+                        'https://www.cortlandstandard.com/stories/guns-drawn-during-arrest,70761?',
+                        ]:
+        scrape_article(article_url,
+                       logged_in_session,
+                       section='Police/Fire', DBsession=DBsession)
+        test_article = DBsession.query(Article).where(
+            Article.url == article_url).first()
 
-    article_content = test_article.content
-    article_date_published = test_article.date_published
-    scrape_unstructured_incident_details(test_article.id, test_article.url, article_content, article_date_published, DBsession)
+        article_content = test_article.content
+        article_date_published = test_article.date_published
+        scrape_unstructured_incident_details(
+            test_article.id, test_article.url, article_content, article_date_published,
+                                             DBsession)
+
     incidents = DBsession.query(Incidents).all()
-    assert len(incidents) == 5
+    assert len(incidents) == 2
