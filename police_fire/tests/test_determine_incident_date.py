26c56891e306f7295a1e5f54ecbd52bbc791903e
kcinnick <nicktucker4@gmail.com>
2023-11-30 23:22:24 -0500

    - fix `test_determine_incident_date.py`

--- police_fire/tests/test_determine_incident_date.py
+++ police_fire/tests/test_determine_incident_date.py
@@ -4,11 +4,13 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from database import Incidents, Base, Persons
 from police_fire.scrape_structured_police_fire_details import check_if_details_references_a_relative_date
-from database import get_database_session, Article, Incidents, Base
+from police_fire.utilities import add_or_get_person
 
 database_username = os.getenv('DATABASE_USERNAME')
 database_password = os.getenv('DATABASE_PASSWORD')
+
 
 @pytest.fixture(scope="function")
 def setup_database():
@@ -20,21 +22,41 @@ def setup_database():
     # Create a new session for testing
     db_session = scoped_session(sessionmaker(bind=engine))
 
+    # add test person for incident
+    fake_person = Persons(name='Fake Person')
+    db_session.add(fake_person)
+    db_session.commit()
+
+    fake_incident = Incidents(
+        accused_person_id=add_or_get_person(db_session, 'Fake Person'),
+        incident_reported_date='2022-11-02',
+        incident_date='2022-10-28',
+        accused_age=52,
+        accused_location='Cortland',
+        charges='Third-degree burglary, a felony; petit larceny, a misdemeanor',
+        details="Heath stole items Friday from Walmart on Bennie Road in Cortlandville, Cortland County sheriff's officers said. She had previously been issued a trespass order barring her from the property.",
+        legal_actions='Heath was arraigned via Cortland County central arraignment and released without bail pending an appearance today in Cortlandville Town Court.',
+        incident_location='Walmart on Bennie Road, Cortlandville, Cortland County',
+        url='https://www.fakeurl.com',
+    )
+
+    db_session.add(fake_incident)
+    db_session.commit()
+
+
     yield db_session  # Provide the session for testing
 
     db_session.close()
     Base.metadata.drop_all(engine)  # Drop tables after tests are done
 
-def test_check_if_details_references_a_relative_date():
-    db_session, engine = get_database_session(environment='test')
-    # get article with ID 184
+
+def test_check_if_details_references_a_relative_date(setup_database):
+    db_session = setup_database
     incident = db_session.query(Incidents).filter(
-        Incidents.url == 'https://www.cortlandstandard.com/stories/syracuse-man-charged-with-burglary-assault,20103?'
-    ).filter(Incidents.accused_name =='Alene D. Heath').first()
+        Incidents.url == 'https://www.fakeurl.com'
+    ).filter(Incidents.accused_person_id == 1).first()
 
     response = check_if_details_references_a_relative_date(incident.details, incident.incident_reported_date)
-    assert response == '2022-03-12'
+    assert response == '2022-10-28'
 
     return
-
-
