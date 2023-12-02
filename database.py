52f3607208b50c5a426ac51c16df70476f7f2a74
kcinnick <nicktucker4@gmail.com>
2023-12-02 15:47:32 -0500

    - remove unnecessary `accused_name` field from `clean_strings_in_table`

--- database.py
+++ database.py
@@ -227,12 +227,11 @@ def remove_non_standard_characters(string):
     return string
 
 
-def clean_strings_in_table(test=False):
-    DBsession, engine = get_database_session(test=test)
+def clean_strings_in_table(environment):
+    DBsession, engine = get_database_session(environment)
     # get the Incidents table
     incidents = DBsession.query(Incidents).all()
     for incident in tqdm(incidents):
-        incident.accused_name = remove_non_standard_characters(incident.accused_name)
         incident.accused_location = remove_non_standard_characters(incident.accused_location)
         incident.charges = remove_non_standard_characters(incident.charges)
         incident.details = remove_non_standard_characters(incident.details)
@@ -242,7 +241,6 @@ def clean_strings_in_table(test=False):
 
     incidents_from_pdf = DBsession.query(IncidentsFromPdf).all()
     for incident in tqdm(incidents_from_pdf):
-        incident.accused_name = remove_non_standard_characters(incident.accused_name)
         incident.accused_location = remove_non_standard_characters(incident.accused_location)
         incident.charges = remove_non_standard_characters(incident.charges)
         incident.details = remove_non_standard_characters(incident.details)
