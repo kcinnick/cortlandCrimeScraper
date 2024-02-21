f084581944b9c2e4784682dd22c25050e22b6010
kcinnick <nicktucker4@gmail.com>
2024-02-21 15:04:53 -0500

    - move `login` function to police_fire/utilities, delete outermost `utilities.py`

--- police_fire/utilities.py
+++ police_fire/utilities.py
@@ -6,6 +6,7 @@ import re
 import sqlalchemy
 from openai import OpenAI
 from openai.types.chat.completion_create_params import ResponseFormat
+from requests import Session
 from tqdm import tqdm
 
 from models.incident import Incident
@@ -178,6 +179,27 @@ def main():
     DBsession, engine = get_database_session(environment='prod')
     link_persons_to_incident(DBsession)
 
+
+def login():
+    session = Session()
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'})
+    session.get('https://www.cortlandstandard.com/login.html')
+
+    login_username = os.getenv('LOGIN_EMAIL')
+    login_password = os.getenv('LOGIN_PASSWORD')
+
+    r = session.post('https://www.cortlandstandard.com/login.html?action=login', data={
+        'login_username': login_username,
+        'login_password': login_password,
+        'referer': '',
+        'ssoreturn': '',
+        'referer_content': '',
+        'login_standard': '',
+    })
+    assert r.status_code == 200
+
+    return session
 
 if __name__ == '__main__':
     main()
