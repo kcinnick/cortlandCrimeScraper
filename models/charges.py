bff44ddc53b7539daebb4df43dcfec851f50aad7
kcinnick <nicktucker4@gmail.com>
2024-02-27 18:59:21 -0500

    - add `counts` to `Charges` model

--- models/charges.py
+++ models/charges.py
@@ -13,8 +13,9 @@ class Charges(Base):
     charge_class = Column(String)  # felony, misdemeanor, violation, traffic_infraction
     degree = Column(String, nullable=True)
     charged_name = Column(String, nullable=True)
+    counts = Column(Integer, nullable=True)
 
     incident_id = Column(Integer, ForeignKey('public.incident.id'))
 
     def __str__(self):
-        return f'{self.charged_name}, {self.charge_description}, {self.charge_class}, {self.degree}, {self.incident_id}'
+        return f'{self.charged_name}, {self.charge_description}, {self.charge_class}, {self.degree}, {self.counts}, {self.incident_id}'
